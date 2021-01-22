14425692532cfe1a70fc8673a9c7cec7fe0ab1a7
Dthomas0729 <slicctech@gmail.com>
2021-01-21 23:53:32 -0500

    CHG: resume data, typos, edu removed

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -5,11 +5,11 @@ class Resume extends Component {
 
     if(this.props.data){
       var skillmessage = this.props.data.skillmessage;
-      var education = this.props.data.education.map(function(education){
-        return <div key={education.school}><h3>{education.school}</h3>
-        <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
-        <p>{education.description}</p></div>
-      })
+      // var education = this.props.data.education.map(function(education){
+      //   return <div key={education.school}><h3>{education.school}</h3>
+      //   <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
+      //   <p>{education.description}</p></div>
+      // })
       var work = this.props.data.work.map(function(work){
         return <div key={work.company}><h3>{work.company}</h3>
             <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
@@ -25,7 +25,7 @@ class Resume extends Component {
     return (
       <section id="resume">
 
-      <div className="row education">
+      {/* <div className="row education">
          <div className="three columns header-col">
             <h1><span>Education</span></h1>
          </div>
@@ -37,7 +37,7 @@ class Resume extends Component {
                </div>
             </div>
          </div>
-      </div>
+      </div> */}
 
 
       <div className="row work">
